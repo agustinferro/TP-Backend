a3b06cc3a1c4fb4ba4ab3a110855b3f272ac7ee6
AgustinFerro <agustinferro4@gmail.com>
2024-06-27 19:24:28 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ("express")
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030 
 
 
 //const productosRouter = require("./routes/productosRouter.js")
