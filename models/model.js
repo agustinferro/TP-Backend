3ca9bf59334692b1e5c932e618a8e2d8823eeb57
AgustinFerro <agustinferro4@gmail.com>
2024-06-28 23:15:39 -0300

    modified:   data/db.js 	modified:   models/model.js

--- models/model.js
+++ models/model.js
@@ -3,12 +3,11 @@ const db = require ("../data/db.js")
 const {DataTypes} = require ("sequelize")
 
 const model = db.define ("Productos",{
-    sku:{type:DataTypes.INTEGER},
     categoria:{type:DataTypes.TEXT},
     producto:{type:DataTypes.TEXT},
     precio: {type:DataTypes.FLOAT},
     puntaje:{type:DataTypes.INTEGER},
-    puntaje:{type:DataTypes.INTEGER},
+    stock:{type:DataTypes.INTEGER},
     
 })
 
