f1a1636d0a2388951576b70d7fd7702e74f744b2
agustinferro <agustinferro4@gmail.com>
2024-07-09 17:53:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use (express.json());
 
 app.get ("/",(req,res)=>{
     res.send(`
-        - GET Todos los registros: grupo1-24127.alwaysdata.net/productos/
-        - GET 1 Registro: grupo1-24127.alwaysdata.net/productos/id}
-        - POST: grupo1-24127.alwaysdata.net/productos/
-        - DELETE: grupo1-24127.alwaysdata.net/productos/id
-        - PUT: grupo1-24127.alwaysdata.net/productos/id
-        Formato de JSON para POST/PUT : 
+        - GET Todos los registros: grupo1-24127.alwaysdata.net/productos/\n
+        - GET 1 Registro: grupo1-24127.alwaysdata.net/productos/id}\n
+        - POST: grupo1-24127.alwaysdata.net/productos/\n
+        - DELETE: grupo1-24127.alwaysdata.net/productos/id\n
+        - PUT: grupo1-24127.alwaysdata.net/productos/id\n
+        Formato de JSON para POST/PUT : \n
         {"categoria":"prueba11","producto":"prueba11","precio":1000.99,"puntaje":4,"stock":600}`);
 });
 
